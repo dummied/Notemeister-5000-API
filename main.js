fcb02bd7c63237e1fe97f0c7ffd96971132e1969
keilsmit <keilsmit@yahoo.com>
2016-11-11 23:49:36 -0500

    refactor1

--- main.js
+++ main.js
@@ -15,43 +15,42 @@ function my_notes() {
 
 my_notes()
 
-function clear_form(selector){
-  $(selector)[0].reset();
+function clear_form(selector) {
+    $(selector)[0].reset();
 }
 
 
 function chat(response) {
-  var display_notes = template(response.note)
-  $('#alpha').prepend(display_notes)
+    var display_notes = template(response.note)
+    $('#alpha').prepend(display_notes)
     alert("hi")
-  clear_form('#create')
+    clear_form('#create')
 }
 
 
-$('#create').on('submit', function(ev){
-  ev.preventDefault()
-    var new_post = $(this).serializeArray()
-    $.post(
-      {
-        url: 'https://infinite-mountain-44724.herokuapp.com/api/notes',
-        data: new_post
-      }).done(function(response){
-        chat(response)
+$('#create').on('submit', function(ev) {
+        ev.preventDefault()
+        var new_post = $(this).serializeArray()
+        $.post({
+            url: 'https://infinite-mountain-44724.herokuapp.com/api/notes',
+            data: new_post
+        }).done(function(response) {
+            chat(response)
+        })
     })
-})
-  // New Post Form ends here
+    // New Post Form ends here
 
 
-  $('#alpha').on('click', '.tag', function(ev){
+$('#alpha').on('click', '.tag', function(ev) {
     ev.preventDefault();
     $('#alpha').html("");
-    $.getJSON('https://infinite-mountain-44724.herokuapp.com/api/notes/tag/'+ encodeURIComponent($(this).html()))
-    .then(function(response){
-      $('#header').html('')
-      $('#header').append( 'Notemeister 5000: ' + response.tag.name);
-      return response.tag.notes.forEach(function(note){
-        var display_notes = template(note)
-        $('#alpha').prepend(display_notes)
-      })
-    })
-  })
+    $.getJSON('https://infinite-mountain-44724.herokuapp.com/api/notes/tag/' + encodeURIComponent($(this).html()))
+        .then(function(response) {
+            $('#header').html('')
+            $('#header').append('Notemeister 5000: ' + response.tag.name);
+            return response.tag.notes.forEach(function(note) {
+                var display_notes = template(note)
+                $('#alpha').prepend(display_notes)
+            })
+        })
+})
